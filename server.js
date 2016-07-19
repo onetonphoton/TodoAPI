cc450c4567b692f7f5850bcf6aeea5ef104d199d
David Austin <daustin@gmail.com>
2016-07-19 08:48:59 -0600

    convert DEL todos to sequelize

--- server.js
+++ server.js
@@ -86,12 +86,27 @@ app.get('/todos/:id', function (req, res){
 
 app.delete('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {id: todoId});
-  if (!matchedTodo) {
-    res.status(404).json({"error":"no todo found with that id"});
-  } else {
-    todos = _.without(todos, matchedTodo);
-    res.json(matchedTodo);
+  // var matchedTodo = _.findWhere(todos, {id: todoId});
+  // if (!matchedTodo) {
+  //   res.status(404).json({"error":"no todo found with that id"});
+  // } else {
+  //   todos = _.without(todos, matchedTodo);
+  //   res.json(matchedTodo);
+  // }
+  db.todo.destroy({
+    where: {
+      id: todoId
+    }
+  }).then(function (rowsDeleted) {
+    if (rowsDeleted === 0) {
+      res.status(404).json({
+        error: 'No todo ID'
+      })
+    } else {
+      res.status(204).send(); //sending a 204 saying everything went well, nothing to return
+    }
+  }), function () {
+    res.status(500).send();
   }
 });
 
